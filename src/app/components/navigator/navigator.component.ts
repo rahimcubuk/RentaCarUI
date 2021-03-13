bbfa9f72864a3bd20a7cf3137b4637b75a547731
rahim çubuk <rahimcubuk@gmail.com>
2021-03-13 13:48:05 +0300

    arayuz guncellendi

--- src/app/components/navigator/navigator.component.ts
+++ src/app/components/navigator/navigator.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Brand } from 'src/app/models/brand/brand';
+import { Color } from 'src/app/models/color/color';
+import { BrandService } from 'src/app/services/brand/brand.service';
+import { ColorService } from 'src/app/services/color/color.service';
 
 @Component({
   selector: 'app-navigator',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigator.component.css']
 })
 export class NavigatorComponent implements OnInit {
-
-  constructor() { }
+  colors: Color[] = [];
+  brands: Brand[] = [];
+  colorLoaded = false;
+  brandLoaded = false;
+  constructor(
+    private colorService: ColorService,
+    private brandService: BrandService
+    ) {}
 
   ngOnInit(): void {
+    this.getColors();
+    this.getBrands();
   }
 
+  getColors() {
+    this.colorService.getColors().subscribe((response) => {
+      this.colors = response.data;
+      this.colorLoaded = true;
+    });
+  }
+    
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
+      this.brandLoaded = true;
+    });
+  }
 }
